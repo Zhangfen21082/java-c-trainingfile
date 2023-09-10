4ec26395a52c845d2737380e2dd4c8bbe6bf5da1
zhangfen21082 <290440677@qq.com>
2023-09-10 21:12:53 +0800

    表白墙服务器版本完成

--- JavaEE-learning/J2023_09_07_onfessionalWallServlet/src/main/java/Message.java
+++ JavaEE-learning/J2023_09_07_onfessionalWallServlet/src/main/java/Message.java
@@ -15,4 +15,16 @@ public class Message {
     public String getContent() {
         return content;
     }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public void setWhom(String whom) {
+        this.whom = whom;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- JavaEE-learning/J2023_09_07_onfessionalWallServlet/src/main/java/MessageServlet.java
+++ JavaEE-learning/J2023_09_07_onfessionalWallServlet/src/main/java/MessageServlet.java
@@ -1,13 +1,29 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+
+/*
+
+show databases;
+create database confession_wall;
+use confession_wall;
+create table message (who varchar(50), whom varchar(50), content varchar(1024));
+
+ */
 
 @WebServlet("/message")
 public class MessageServlet extends HttpServlet {
@@ -17,6 +33,15 @@ public class MessageServlet extends HttpServlet {
     // 让页面获取到数据
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 声明当前的响应数据格式
+        resp.setContentType("application/json; charset=utf-8");
+        // 读取数据库内容返回给页面
+
+        try {
+            messageList = load();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         resp.getWriter().write(objectMapper.writeValueAsString(messageList));
     }
 
@@ -25,9 +50,88 @@ public class MessageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 获取到body中的数据并解析
         Message message = objectMapper.readValue(req.getInputStream(), Message.class);
-        messageList.add(message);
+        // 向数据库提交内容
+        try {
+            save(message);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         resp.setStatus(200);
         System.out.println("成功提交数据："
                 + "【" + message.getWho() + "】" + "对【" + message.getWhom() + "】" + "说：" + message.getContent());
+
+    }
+
+    // 从数据库查询数据
+    private List<Message> load() throws SQLException {
+        // 先得有一个数据源
+        DataSource dataSource = new MysqlDataSource();
+        // 设置url，user和password
+        ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/confession_wall?characterEncoding=utf8&useSSL=false&serverTimezone=UTC");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("zxsgdsg6125057");
+        // 与服务器建立连接
+        Connection connection = dataSource.getConnection();
+
+
+        // 构造SQL并预处理
+        String sql = "select * from message";
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+
+        // 执行SQL得到结果集合
+        ResultSet resultSet = statement.executeQuery();
+
+        //遍历结果集合
+        // List<Message> messageList = new ArrayList<>();
+        while (resultSet.next()) {
+            Message message = new Message();
+            message.setWho(resultSet.getString("who"));
+            message.setWhom(resultSet.getString("whom"));
+            message.setContent(resultSet.getString("content"));
+            messageList.add(message);
+
+        }
+
+        // 关闭连接释放资源
+        statement.close();
+        connection.close();
+
+        return messageList;
+    }
+
+    // 保存数据到数据库
+    private void save(Message message) throws SQLException {
+        // 先得有一个数据源
+        DataSource dataSource = new MysqlDataSource();
+        // 设置url，user和password
+        ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/confession_wall?characterEncoding=utf8&useSSL=false&serverTimezone=UTC");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("zxsgdsg6125057");
+        // 与服务器建立连接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+
+        // 构造SQL并进行预处理
+        String sql = "insert into message values(?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        System.out.println(sql);
+        statement.setString(1, message.who);
+        statement.setString(2, message.whom);
+        statement.setString(3, message.content);
+
+        // 执行SQL然后插入
+        try {
+            int res = statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // 关闭连接释放资源
+        statement.close();
+        connection.close();
+
+
+
     }
 }
