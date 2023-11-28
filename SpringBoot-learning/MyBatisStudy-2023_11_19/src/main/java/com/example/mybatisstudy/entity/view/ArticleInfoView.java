5021f203872cc7ad78344d4fb5bd995d9e464848
Zhangfen21082 <290440677@qq.com>
2023-11-28 16:50:57 +0800

    11-28

--- SpringBoot-learning/MyBatisStudy-2023_11_19/src/main/java/com/example/mybatisstudy/entity/view/ArticleInfoView.java
+++ SpringBoot-learning/MyBatisStudy-2023_11_19/src/main/java/com/example/mybatisstudy/entity/view/ArticleInfoView.java
@@ -3,8 +3,17 @@ package com.example.mybatisstudy.entity.view;
 import com.example.mybatisstudy.entity.ArticleInfo;
 import lombok.Data;
 
+import java.io.Serializable;
+
 
 @Data
-public class ArticleInfoView extends ArticleInfo {
+public class ArticleInfoView extends ArticleInfo implements Serializable {
     private String username;
+
+    @Override
+    public String toString() {
+        return "ArticleInfoView{" +
+                "username='" + username + '\'' +
+                "} " + super.toString();
+    }
 }
